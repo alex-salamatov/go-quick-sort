d6500c0bb78fad2b230c70f8b5bb979e92e6cd1d
alex-salamatov <alex.salamatov@gmail.com>
2026-01-30 09:35:27 +0100

    Optimized placing of pivot to its position

--- main.go
+++ main.go
@@ -35,44 +35,42 @@ func QuickSort(container []int) {
 		}
 	}
 
-	firstRightPivotIndex := sortedPivotIndex + 1
-
-	if sortedPivotIndex == 0 { // special case, sorted pivot position is a leftmost element in sorted sequence, so there are no elements on the left of pivot
-		swap(&container[0], &container[pivotIndex])
-		QuickSort(container[1:containerLen]) //sort elements to the right of pivot
-		return
-	}
-
-	if firstRightPivotIndex == containerLen { // special case, sorted pivot position is a rightmost element in sorted sequence, so there are no elements to the right of pivot
-		swap(&container[containerLen-1], &container[pivotIndex])
-		QuickSort(container[0 : containerLen-1])
-		return
+	if pivotIndex != sortedPivotIndex {
+		swap(&container[pivotIndex], &container[sortedPivotIndex]) //pivot is placed to its place
 	}
 
 	//fmt.Printf("  sortedPivotIndex = %d, N LeftElements = %d N RightElements = %d \n", sortedPivotIndex, sortedPivotIndex, containerLen-sortedPivotIndex-1)
 
-	left := container[0:sortedPivotIndex]                 //elements on the left + pivots
-	right := container[firstRightPivotIndex:containerLen] //elements on the right of pivot
+	var left, right []int
 
-	swap(&container[pivotIndex], &container[sortedPivotIndex]) //pivot is placed to its place
+	if sortedPivotIndex > 0 {
+		left = container[0:sortedPivotIndex] //elements on the left of pivot
+	}
 
-	for i, jRight := 0, 0; i < sortedPivotIndex; i++ {
+	if sortedPivotIndex < containerLen-1 {
+		right = container[sortedPivotIndex+1 : containerLen] //elements on the right of pivot
+	}
 
-		if left[i] < pivot {
-			continue //left[i] can stay in its position on the left of pivot
-		}
+	if left != nil && right != nil {
+		//start moving elements from left to right parts if needed
+		for i, jRight := 0, 0; i < sortedPivotIndex; i++ {
 
-		for k := jRight; k < len(right); k++ {
+			if left[i] < pivot {
+				continue //left[i] can stay in its position on the left of pivot
+			}
 
-			if right[k] < pivot {
-				swap(&left[i], &right[k])
-				jRight = k + 1 //index of element of the right from pivot slice, starting from which there still might be elements to be moved to the left of pivot
-				break
+			for k := jRight; k < len(right); k++ {
+
+				if right[k] < pivot {
+					swap(&left[i], &right[k])
+					jRight = k + 1 //index of element of the right from pivot slice, starting from which there still might be elements to be moved to the left of pivot
+					break
+				}
 			}
 		}
 	}
 
-	//fmt.Printf("Output: %v - %d - %v\n", left, pivot, right)
+	//	fmt.Printf("Output: %v - %d - %v\n", left, pivot, right)
 
 	QuickSort(left)  //apply quick sort to the left  from pivot part of container
 	QuickSort(right) //apply quick sort to the right from pivot part of container
@@ -81,7 +79,8 @@ func QuickSort(container []int) {
 func main() {
 	fmt.Println("Quick sort")
 
-	collection := []int{13, 4, 35, 104, 28, 6, 12, 200, 1, -5, 44, 55, 11, 99, -9, 21, -100, 67, -37, 12, -8, 1, 99, 99, 150, 6, -120}
+	collection := []int{-250, 13, 4, 35, 104, 28, 6, 12, 200, 1, -5, 44, 55, 11, 99, -9, 21, -100, 67, -37, 12, -8, 1, 99, 99, 150, 6, -120, -100, 7}
+	//	collection := []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	fmt.Printf("Initial container = %v\n", collection)
 	QuickSort(collection)
 	fmt.Printf("Sorted container  = %v\n", collection)
