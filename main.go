f13d9b7b41f972a601066fbaeff9351754cc31c0
alex-salamatov <alex.salamatov@gmail.com>
2026-02-02 13:03:23 +0100

    Renamed package to sorting

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"go_quick_sort/pkg/sort"
+
+	"github.com/alex-salamatov/go_quick_sort/pkg/sorting"
 )
 
 func main() {
 	fmt.Println("Quick sort")
 
 	collection := []int{-250, 13, 4, 35, 104, 28, 6, 12, 200, 1, -5, 44, 55, 11, 99, -9, 21, -100, 67, -37, 12, -8, 1, 99, 99, 150, 6, -120, -100, 7}
-	//	collection := []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	//collection := []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	fmt.Printf("Initial container = %v\n", collection)
-	sort.QuickSort(collection)
+	sorting.QuickSort(collection)
 	fmt.Printf("Sorted container  = %v\n", collection)
 }
